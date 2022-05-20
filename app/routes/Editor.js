b5a82cfd701f7aa378425f3009a5fbb93c753419
jezpoz <jesfor94@gmail.com>
2022-05-20 22:28:41 +0200

    basic markdown editor done

--- app/routes/Editor.js
+++ app/routes/Editor.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+
+const LOCALSTORAGE_MARKDOWN_KEY = "MARKDOWN";
+
 export default function Editor() {
+  const [markdown, setMarkdown] = useState('# Write some markdown');
+
+  function saveMarkdown() {
+    return localStorage.setItem(LOCALSTORAGE_MARKDOWN_KEY, markdown);
+  }
+
+  function loadMarkdown() {
+    return localStorage.getItem(LOCALSTORAGE_MARKDOWN_KEY);
+  }
+
+  function handleMarkdownChange(e) {
+    setMarkdown(e.target.value.toString());
+  }
+
+  useEffect(() => {
+    setMarkdown(loadMarkdown());
+  }, [])
+
   return (
     <main>
       <h1>Editor</h1>
+      <p>Write some markdown, click on the save button and <a href="/preview">preview</a> it</p>
+
+      <textarea value={markdown} onChange={handleMarkdownChange}/>
+      <button onClick={saveMarkdown}>Save</button>
     </main>
   );
 }
